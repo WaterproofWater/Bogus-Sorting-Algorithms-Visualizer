9f431b3cf63d5dc6e1bd93efcfe015208404916a
Andy Tran <aandy.tran@mail.utoronto.ca>
2024-08-29 15:39:59 -0700

    Centered the array to the page and fixed the proportion per bar.

--- sort.js
+++ sort.js
@@ -1,4 +1,4 @@
-const arr_size = 10;
+const arr_size = 30; // Maybe cap at 100 (slider)
 const arr = [];
 
 for (let i = 0; i < arr_size; i++){
@@ -8,8 +8,9 @@ for (let i = 0; i < arr_size; i++){
 for (let i = 0; i < arr.length; i++){
     const bar = document.createElement("div");
     bar.style.height = arr[i] * 100 + "%";
-    bar.style.width = arr_size + "px";          // Change this to reverse of this logic (more numbers should make the width less)
+    bar.style.width = (110 - arr_size) + "px";          // Change this to reverse of this logic (more numbers should make the width less)
     bar.style.backgroundColor = "black";
+    bar.style.margin = "1px";
     container.appendChild(bar);
 }
 
